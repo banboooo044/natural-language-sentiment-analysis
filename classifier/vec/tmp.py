c658ec0c3b03be12d814a5951710e3c41d8fe05e
banboooo044 <33150569+banboooo044@users.noreply.github.com>
2019-12-19 20:36:53 +0900

    Delete tmp.py

--- classifier/vec/tmp.py
+++
@@ -1,7 +0,0 @@
-
-import pandas as pd
-PATH = "../data/train-val-wakati-juman.tsv"
-df = pd.read_table(PATH, index_col=0)
-df = df[~pd.isnull(df["text"])]
-
-token = df["text"].apply(lambda x: x.split(","))
